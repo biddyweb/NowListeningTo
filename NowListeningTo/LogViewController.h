6a1e4a8bb334382e466684ed89ff92a6840125ac
Ezequiel Alejandro Becerra <ezequiel.becerra@gmail.com>
2012-10-19 12:05:41 -0300

    Settings on the upper left now

---
+++ NowListeningTo/LogViewController.h
@@ -0,0 +1,14 @@
+//
+//  LogViewController.h
+//  NowListeningTo
+//
+//  Created by Ezequiel A Becerra on 10/19/12.
+//  Copyright (c) 2012 Betzerra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LogViewController : UIViewController
+- (IBAction)doneButtonTapped:(id)sender;
+
+@end
---
+++ NowListeningTo/LogViewController.m
@@ -0,0 +1,41 @@
+//
+//  LogViewController.m
+//  NowListeningTo
+//
+//  Created by Ezequiel A Becerra on 10/19/12.
+//  Copyright (c) 2012 Betzerra. All rights reserved.
+//
+
+#import "LogViewController.h"
+
+@interface LogViewController ()
+
+@end
+
+@implementation LogViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)doneButtonTapped:(id)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+@end
