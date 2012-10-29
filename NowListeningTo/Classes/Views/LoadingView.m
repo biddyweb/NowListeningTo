6fbd044306a2b579d8e2fb27c1dcadb5cf32cf72
Ezequiel Alejandro Becerra <ezequiel.becerra@gmail.com>
2012-10-28 21:12:53 -0300

    Added animation on loading view

--- NowListeningTo/Classes/Views/LoadingView.m
+++ NowListeningTo/Classes/Views/LoadingView.m
@@ -16,6 +16,39 @@
 
 #pragma mark - Private
 
+//  Found on http://stackoverflow.com/questions/2690775/creating-a-pop-animation-similar-to-the-presentation-of-uialertview
+- (void) attachPopUpAnimation{
+    CAKeyframeAnimation *animation = [CAKeyframeAnimation
+                                      animationWithKeyPath:@"transform"];
+    
+    CATransform3D scale1 = CATransform3DMakeScale(0.5, 0.5, 1);
+    CATransform3D scale2 = CATransform3DMakeScale(1.2, 1.2, 1);
+    CATransform3D scale3 = CATransform3DMakeScale(0.9, 0.9, 1);
+    CATransform3D scale4 = CATransform3DMakeScale(1.0, 1.0, 1);
+    
+    NSArray *frameValues = [NSArray arrayWithObjects:
+                            [NSValue valueWithCATransform3D:scale1],
+                            [NSValue valueWithCATransform3D:scale2],
+                            [NSValue valueWithCATransform3D:scale3],
+                            [NSValue valueWithCATransform3D:scale4],
+                            nil];
+    [animation setValues:frameValues];
+    
+    NSArray *frameTimes = [NSArray arrayWithObjects:
+                           [NSNumber numberWithFloat:0.0],
+                           [NSNumber numberWithFloat:0.5],
+                           [NSNumber numberWithFloat:0.9],
+                           [NSNumber numberWithFloat:1.0],
+                           nil];
+    [animation setKeyTimes:frameTimes];
+    
+    animation.fillMode = kCAFillModeForwards;
+    animation.removedOnCompletion = NO;
+    animation.duration = .2;
+    
+    [self.layer addAnimation:animation forKey:@"popup"];
+}
+
 #pragma mark - Public
 
 - (id)initWithSuperView:(UIView *)aView{
@@ -29,6 +62,7 @@
     self = [self initWithFrame:aRect];
     if (self){
         [aView setUserInteractionEnabled:NO];
+        [self attachPopUpAnimation];
     }
     return self;
 }
