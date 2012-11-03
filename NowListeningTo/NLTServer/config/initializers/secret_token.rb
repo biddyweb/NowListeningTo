854e2aa3f8c90408c3757c16efaf3e18fe84e545
Ezequiel Alejandro Becerra <ezequiel.becerra@gmail.com>
2012-11-03 01:21:20 -0300

    Base ruby conf

---
+++ NowListeningTo/NLTServer/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NLTServer::Application.config.secret_token = 'f03aa44907e95887852bbc4322973fb144fdbd4d1c2e049937e72efa7adbc9bae3552785be59943cdfdf077554c75b576070b55e03107fd6a5f4e44e97073e51'
